662b567bb61847bca8255958bb3202ba3f4d3a18
Caia Chuang <caiachuang@gmail.com>
2021-05-20 22:30:26 -0700

    Modify Product.

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -15,10 +15,10 @@ const Home = () => {
                 <div className="home__row">
                     <Product
                         id="12321341"
-                        title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
-                        price={11.96}
+                        title="Cracking the Coding Interview: 189 Programming Questions and Solutions Paperback – Illustrated, July 1 2015k"
+                        price={33.91}
                         rating={5}
-                        image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+                        image="https://images-na.ssl-images-amazon.com/images/I/41oYsXjLvZL._SX348_BO1,204,203,200_.jpg"
                     />
                     <Product
                         id="49538094"
@@ -41,14 +41,14 @@ const Home = () => {
                         id="23445930"
                         title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
                         price={98.99}
-                        rating={5}
+                        rating={4}
                         image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
                     />
                     <Product
                         id="3254354345"
                         title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
                         price={598.99}
-                        rating={4}
+                        rating={5}
                         image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
                     />
                 </div>
@@ -58,7 +58,7 @@ const Home = () => {
                         id="90829332"
                         title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
                         price={1094.98}
-                        rating={4}
+                        rating={3}
                         image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg"
                     />
                 </div>
