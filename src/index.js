c408af9ee2d1d43252e4fd738be730ac45f10d23
Caia Chuang <caiachuang@gmail.com>
2021-05-25 18:57:24 -0700

    Add reducer.js.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { StateProvider } from "./StateProvider";
+import reducer, { initialState } from "./reducer";
 
 ReactDOM.render(
     <React.StrictMode>
