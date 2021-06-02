abd45eed2aa433979dae2b8919af73b0b12dc5d5
Caia Chuang <caiachuang@gmail.com>
2021-06-01 21:34:23 -0700

    Dispatch the items to the basket.

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { useStateValue } from '../../StateProvider';
 import './Product.css';
 
 const Product = ({ id, title, image, price, rating }) => {
+    const [{ basket }, dispatch] = useStateValue();
+
+    console.log('basket', basket)
+
+    const addToBasket = () => {
+        // dispatch the item into the data layer
+        dispatch({
+            type: 'ADD_TO_BASKET',
+            item: {
+                id,
+                title,
+                image,
+                price,
+                rating,
+            }
+        })
+    }
+
     return (
         <div className="product">
             <div className="product__info">
@@ -21,7 +40,7 @@ const Product = ({ id, title, image, price, rating }) => {
                 src={image}
                 alt={title}
             />
-            <button>Add to Basket</button>
+            <button onClick={addToBasket}>Add to Basket</button>
         </div>
     );
 }
