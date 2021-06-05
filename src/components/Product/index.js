ad7589ec7efecad2ca10b2270893106aed4e7f52
Caia Chuang <caiachuang@gmail.com>
2021-06-05 14:31:26 -0700

    Build & style CheckoutProduct.

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { useStateValue } from '../../StateProvider';
-import './Product.css';
+import React from "react";
+import { useStateValue } from "../../StateProvider";
+import "./Product.css";
 
 const Product = ({ id, title, image, price, rating }) => {
     const [{ basket }, dispatch] = useStateValue();
@@ -9,16 +9,16 @@ const Product = ({ id, title, image, price, rating }) => {
     const addToBasket = () => {
         // dispatch the item into the data layer
         dispatch({
-            type: 'ADD_TO_BASKET',
+            type: "ADD_TO_BASKET",
             item: {
                 id,
                 title,
                 image,
                 price,
                 rating,
-            }
-        })
-    }
+            },
+        });
+    };
 
     return (
         <div className="product">
@@ -29,19 +29,18 @@ const Product = ({ id, title, image, price, rating }) => {
                     <strong>{price}</strong>
                 </p>
                 <div className="product__rating">
-                    {Array(rating).fill().map((_, i) => (
-                        <p>⭐️</p>
-                    ))}
+                    {Array(rating)
+                        .fill()
+                        .map((_, i) => (
+                            <p>⭐️</p>
+                        ))}
                 </div>
             </div>
 
-            <img
-                src={image}
-                alt={title}
-            />
+            <img src={image} alt={title} />
             <button onClick={addToBasket}>Add to Basket</button>
         </div>
     );
-}
+};
 
 export default Product;
