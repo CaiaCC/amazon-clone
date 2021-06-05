cfef94180bebe71764c929757382d1efe81b8350
Caia Chuang <caiachuang@gmail.com>
2021-06-05 15:18:58 -0700

    Add signIn and register function.

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import './Login.css';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Login.css";
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const signIn = e => {
+        e.preventDefault();
+
+    };
+
+    const register = e => {
+        e.preventDefault();
+        
+    }
+
     return (
         <div className="login">
             <Link to="/">
@@ -18,26 +31,40 @@ const Login = () => {
 
                 <form>
                     <h5>E-mail</h5>
-                    <input type="text" />
+                    <input
+                        type="text"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
 
                     <h5>Password</h5>
-                    <input type="password" />
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
 
-                    <button className="login__signInButton">Sign In</button>
+                    <button
+                        className="login__signInButton"
+                        type="submit"
+                        onClick={signIn}
+                    >
+                        Sign In
+                    </button>
                 </form>
-                
+
                 <p>
                     By singing-in you agree to the AMAZON CLONE Conditions of
                     Use & Sale. Please see our Privacy Notice, our Cookies
                     Notice and our Interest-Based Ads
                 </p>
 
-                <button className="login__registerButton">
+                <button className="login__registerButton" onClick={register} >
                     Create your Amazon Clone account
                 </button>
             </div>
         </div>
     );
-}
+};
 
 export default Login;
