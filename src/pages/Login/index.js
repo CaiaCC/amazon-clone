00cc448e2af7d6389e1dd8fe2a8824afc0066137
Caia Chuang <caiachuang@gmail.com>
2021-06-05 14:56:19 -0700

    Build Login page.

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -12,6 +12,30 @@ const Login = () => {
                     alt="Amazon logo"
                 />
             </Link>
+
+            <div className="login__container">
+                <h1>Sign in</h1>
+
+                <form>
+                    <h5>E-mail</h5>
+                    <input type="text" />
+
+                    <h5>Password</h5>
+                    <input type="password" />
+
+                    <button className="login__signInButton">Sign In</button>
+                </form>
+                
+                <p>
+                    By singing-in you agree to the AMAZON CLONE Conditions of
+                    Use & Sale. Please see our Privacy Notice, our Cookies
+                    Notice and our Interest-Based Ads
+                </p>
+
+                <button className="login__registerButton">
+                    Create your Amazon Clone account
+                </button>
+            </div>
         </div>
     );
 }
