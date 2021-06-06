f614457e6b4281857d73f50fbe05b592b2fd79a9
Caia Chuang <caiachuang@gmail.com>
2021-06-05 21:16:32 -0700

    Style Payment component.

--- src/pages/Payment/index.js
+++ src/pages/Payment/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import './Payment.css';
-import {useStateValue} from '../../StateProvider';
-import CheckoutProduct from '../../components/CheckoutProduct';
+import React from "react";
+import "./Payment.css";
+import { useStateValue } from "../../StateProvider";
+import CheckoutProduct from "../../components/CheckoutProduct";
+import { Link } from "react-router-dom";
 
 const Payment = () => {
-    const[{user, basket}, dispatch] = useStateValue();
+    const [{ user, basket }, dispatch] = useStateValue();
     return (
         <div className="payment">
             <div className="payment__container">
+                <h1>
+                    Checkout (<Link to="/checkout">{basket?.length} items</Link>
+                    )
+                </h1>
+
                 <div className="payment__section">
                     <div className="payment__title">
                         <h3>Delivery Address</h3>
@@ -18,7 +24,7 @@ const Payment = () => {
                         <p>BC, Canada</p>
                     </div>
                 </div>
-                
+
                 <div className="payment__section">
                     <div className="payment__title">
                         <h3>Review items and delivery</h3>
@@ -40,13 +46,11 @@ const Payment = () => {
                     <div className="payment__title">
                         <h3>Payment method</h3>
                     </div>
-                    <div className="payment__details">
-
-                    </div>
+                    <div className="payment__details"></div>
                 </div>
             </div>
         </div>
     );
-}
+};
 
 export default Payment;
