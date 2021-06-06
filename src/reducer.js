dbad47d8ae13d0420853699f8e9ba83ee5153f0c
Caia Chuang <caiachuang@gmail.com>
2021-06-05 19:00:46 -0700

    Add SET_USER action in the reducer.

--- src/reducer.js
+++ src/reducer.js
@@ -4,8 +4,7 @@ export const initialState = {
 };
 
 // selector
-export const getBasketTotal = (basket) =>
-    basket?.reduce((amount, item) => item.price + amount, 0);
+export const getBasketTotal = (basket) => basket?.reduce((amount, item) => item.price + amount, 0);
 
 const reducer = (state, action) => {
     // console.log(action);
@@ -30,6 +29,11 @@ const reducer = (state, action) => {
                 );
             }
             return { ...state, basket: newBasket };
+        case "SET_USER":
+            return {
+                ...state,
+                user: action.user,
+            };
         default:
             return state;
     }
