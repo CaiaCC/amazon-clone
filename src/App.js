0e068791fbae0a5933cdb46e870ce8f2d3b1206b
Caia Chuang <caiachuang@gmail.com>
2021-06-05 19:01:18 -0700

    Store user in the data layer.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import Checkout from './pages/Checkout';
-import Login from './pages/Login';
+import Checkout from "./pages/Checkout";
+import Login from "./pages/Login";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
 
 const App = () => {
+    const [{}, dispatch] = useStateValue();
+
+    useEffect(() => {
+        auth.onAuthStateChanged((authUser) => {
+            console.log("the user is: ", authUser);
+
+            if (authUser) {
+                dispatch({
+                    type: "SET_USER",
+                    user: authUser,
+                });
+            } else {
+                dispatch({
+                    type: "SET_USER",
+                    user: null,
+                });
+            }
+        });
+    }, []);
+
     return (
         <Router>
             <div className="app">
