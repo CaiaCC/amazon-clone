68eb7bbba1be7ea6ada40ba25ea3c30dafbb1a53
Caia Chuang <caiachuang@gmail.com>
2021-06-05 19:48:18 -0700

    Show user email on checkout page.

--- src/pages/Checkout/index.js
+++ src/pages/Checkout/index.js
@@ -5,7 +5,7 @@ import CheckoutProduct from "../../components/CheckoutProduct";
 import { useStateValue } from "../../StateProvider";
 
 const Checkout = () => {
-    const [{ basket }, patch] = useStateValue();
+    const [{ basket, user }, dispatch] = useStateValue();
 
     return (
         <div className="checkout">
@@ -13,10 +13,11 @@ const Checkout = () => {
                 <img
                     className="checkout__ad"
                     src="https://images-na.ssl-images-amazon.com/images/G/15/dex/2020/PrimeDelivery/DEX_2020_PrimeDelivery_LP_CA_0407_Header_Desktop_3000x600.jpg"
-                    alt="Currency Converter ad banner"
+                    alt="Currency Converter advertise banner"
                 />
 
                 <div>
+                <h3>Hello, {user?.email}</h3>
                     <h2 className="checkout__title">Your shopping Basket</h2>
 
                     {basket.map(item => (
